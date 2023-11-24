565d40f393eff4e5681f0840563660130603eec8
Khwez1 <khwezimbobo@gmail.com>
2023-11-24 14:26:14 +0200

    fixed some mistakes

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 //Declaring variables
 let Output = document.getElementById('output');
 let Btn = document.getElementById('btn');
+let i = 0
 //Creating array
 let numArray = [1, 4, 7, 11, 22, 44, 77, 99, 101, 111];
 //function, if i is less than the array length then it will cycle through the array
-function nextNum() {
-    if(i = 0,i<numArray.length,i++){
+function nextNum(){
+    if(i<numArray.length,i++){
     next = numArray[i];Output.value = next
     }
 }
