d42f96d4c569e08d344cf6460d8bca3d2a43060d
Khwez1 <khwezimbobo@gmail.com>
2023-11-24 14:18:17 +0200

    added some files

---
+++ index.js
@@ -0,0 +1,12 @@
+//Declaring variables
+let Output = document.getElementById('output');
+let Btn = document.getElementById('btn');
+//Creating array
+let numArray = [1, 4, 7, 11, 22, 44, 77, 99, 101, 111];
+//function, if i is less than the array length then it will cycle through the array
+function nextNum() {
+    if(i = 0,i<numArray.length,i++){
+    next = numArray[i];Output.value = next
+    }
+}
+Btn.addEventListener('click', nextNum)
